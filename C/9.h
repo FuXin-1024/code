1b7899f33a7c6ba88b9e449af3a879ee7101e506
FuXin-1024 <1007160080@qq.com>
2017-07-05 21:47:05 +0800

    1-100数字9出现的次数

---
+++ C/9.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include<stdlib.h>
+#include<stdio.h>
+
+//编写程序数⼀下 1到 100 的所有整数中出现多少次数字9
+
+void count_nine()
+{
+	int num = 1;
+	int count = 0;
+	for (; num <= 100; num++)
+	{
+		if (num % 10 == 9)
+			count++;
+		if (num / 10 == 9)
+			count++;
+	}
+	printf("count = %d\n", count);
+}
+
+void Test9()
+{
+	count_nine();
+}
