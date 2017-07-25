0d9cce34e217ee0a01dbe27a04cdfff63b0c0a36
FuXin-1024 <1007160080@qq.com>
2017-07-25 17:01:08 +0800

    二叉树的前序，中序，后序遍历

--- code/BinaryTreeOrder.h
+++ code/BinaryTreeOrder.h
@@ -1,5 +1,6 @@
 #pragma once
 #include<iostream>
+#include<stack>
 using namespace std;
 
 struct BinaryTreeNode
@@ -38,6 +39,26 @@ void PrevOrder(Node* root)
 	PrevOrder(root->_right);
 }
 
+void PrevOrderNonR(Node* root)
+{
+	if (root == NULL)
+		return;
+	Node* cur = root;
+	stack<Node*> s;
+	while (cur || !s.empty())
+	{
+		while (cur)
+		{
+			s.push(cur);
+			cout << cur->_value << " ";
+			cur = cur->_left;
+		}
+		Node* top = s.top();
+		s.pop();
+		cur = top->_right;
+	}
+}
+
 void InOrder(Node* root)
 {
 	if (root == NULL)
@@ -47,6 +68,25 @@ void InOrder(Node* root)
 	InOrder(root->_right);
 }
 
+void InOrderNonR(Node* root)
+{
+	if (root == NULL)
+		return;
+	Node* cur = root;
+	stack<Node*> s;
+	while (cur || !s.empty())
+	{
+		while (cur)
+		{
+			s.push(cur);
+			cur = cur->_left;
+		}
+		Node* top = s.top();
+		cout << top->_value << " ";
+		s.pop();
+		cur = top->_right;
+	}
+}
 void PostOrder(Node* root)
 {
 	if (root == NULL)
@@ -55,12 +95,45 @@ void PostOrder(Node* root)
 	PostOrder(root->_right);
 	cout << root->_value << " ";
 }
+
+void PostOrderNonR(Node* root)
+{
+	if (root == NULL)
+		return;
+	Node* cur = root;
+	Node* prev = NULL;
+	stack<Node*> s;
+	while (cur || !s.empty())
+	{
+		while (cur)
+		{
+			s.push(cur);
+			cur = cur->_left;
+		}
+		Node* top = s.top();
+		if (top->_right == NULL || top->_right == prev)
+		{
+			cout << top->_value << " ";
+			prev = top;
+			s.pop();
+		}
+		else
+		{
+			cur = top->_right;
+		}
+	}
+}
+
 void OrderTest()
 {
 	int  a[] = { 1, 2, 3, '#', '#', 4, '#', '#',5,6 };
 	size_t index = 0;
 	Node* root = CreateTree(a, sizeof(a) / sizeof(a[0]), index);
 	cout << "PrevOrder: "; PrevOrder(root); cout << endl;
+	cout << "PrevOrderNonR: "; PrevOrderNonR(root); cout << endl;
 	cout << "InOrder: "; InOrder(root); cout << endl;
+	cout << "InOrderNonR: "; InOrderNonR(root); cout << endl;
 	cout << "PostOrder: "; PostOrder(root); cout << endl;
+	cout << "PostOrderNonR: "; PostOrderNonR(root); cout << endl;
+
 }
