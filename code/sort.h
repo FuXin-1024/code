f995647e01bf7f726188808a4bc6859e0a30eee6
FuXin-1024 <1007160080@qq.com>
2017-07-31 09:48:13 +0800

    希尔排序

--- code/sort.h
+++ code/sort.h
@@ -1,8 +1,12 @@
 #include<iostream>
+#include<assert.h>
 using namespace std;
 
+//Ö±½Ó²åÈëÅÅÐò
 void InsertSort(int* arr, int len)//ÉýÐòÅÅÁÐ
 {
+	assert(arr);
+	assert(len > 0);
 	for (int i = 1; i <len; i++)
 	{
 		int temp = arr[i];
@@ -16,8 +20,37 @@ void InsertSort(int* arr, int len)//ÉýÐòÅÅÁÐ
 		arr[j + 1] = temp;
 	}
 }
+
+void ShellSort(int* arr, int len)
+{
+	assert(arr);
+	assert(len);
+
+	int gap = len;
+	while (gap > 1)
+	{
+		gap = gap / 3 + 1;
+		for (int i = gap; i < len; i++)
+		{
+			int end = i - gap;
+			int temp = arr[i];
+
+			while (end >= 0)
+			{
+				if (arr[end] < temp)
+					break;
+
+				arr[end + gap] = arr[end];
+				end -= gap;
+			}
+			arr[end + gap] = temp;
+		}
+	}
+}
 void Test()
 {
 	int arr[] = { 5 ,8, 9, 4 };
 	InsertSort(arr, sizeof(arr) / sizeof(arr[0]));
+	int arr1[] = { 3, 1, 5, 2, 8, 7, 9, 0, 6, 4 };
+	ShellSort(arr1, sizeof(arr1) / sizeof(arr1[0]));
 }
