f3fb91837a4bc351161d8474c2a9d628a6011b4b
FuXin-1024 <1007160080@qq.com>
2017-08-17 14:21:01 +0800

    二叉树

--- code/BinaryTree.h
+++ code/BinaryTree.h
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<stack>
 #include<queue>
+#include<assert.h>
 using namespace std;
 
 template <class T>
@@ -157,6 +158,30 @@ public:
 	{
 		return  _GetLeafSize(_root);
 	}
+
+	size_t GetLeafSize1()
+	{
+		size_t count = 0;
+		_GetLeafSize(_root, count);
+		return count;
+	}
+
+	//求第k层的节点数
+	size_t GetKLevelSize(size_t k)
+	{
+		assert(k > 0);
+		return _GetKLevelSize(_root, k);
+	}
+
+	size_t Depth()
+	{
+		return _Depth(_root);
+	}
+
+	Node* Find(const T& x)
+	{
+		return _Find(_root, x);
+	}
 protected:
 	Node* CreateTree(T* a, size_t n, const T& invalid, size_t& index)
 	{
@@ -240,7 +265,48 @@ protected:
 		return _GetLeafSize(root->_left) + _GetLeafSize(root->_right);
 	}
 
+	void _GetLeafSize(Node* root, size_t& count)
+	{
+		if (root == NULL)
+			return ;
 
+		if (root->_left == NULL && root->_right == NULL)
+			count++;
+
+		_GetLeafSize(root->_left,count);
+		_GetLeafSize(root->_right,count);
+	}
+
+	size_t _GetKLevelSize(Node* root, size_t k)
+	{
+		if (root == NULL)
+			return 0;
+		if (k == 1)
+			return 1;
+		return _GetKLevelSize(root->_left, k - 1) + _GetKLevelSize(root->_right, k - 1);
+	}
+
+	size_t _Depth(Node* root)
+	{
+		if (root == NULL)
+			return 0;
+		int left = _Depth(root->_left);
+		int right = _Depth(root->_right);
+		return left > right ? left + 1 : right + 1;
+	}
+
+	Node* _Find(Node* root, const T& x)
+	{
+		if (root == NULL)
+			return NULL;
+		if (root->_data == x)
+			return root;
+
+		Node* ret = _Find(root->_left, x);
+		if (ret)
+			return ret;
+		return _Find(root->_right, x);
+	}
 
 	void _Destroy(Node* root)
 	{
@@ -287,4 +353,11 @@ void BinaryTreeTest()
 	cout << "Size = ";  cout << t.Size()<< endl;
 
 	cout << "LeafSize = ";  cout << t.GetLeafSize() << endl;
+	cout << "LeafSize = ";  cout << t.GetLeafSize1() << endl;
+
+	cout << "KLevelSize = ";  cout << t.GetKLevelSize(3) << endl;
+
+	cout << "Depth = ";  cout << t.Depth() << endl;
+	
+	cout << "Find(3) = "; cout << t.Find(3)->_data << endl;
 }
