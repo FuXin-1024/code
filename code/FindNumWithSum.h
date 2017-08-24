b71b9b25595c8e55176344d60a7c4e00d1c65b26
FuXin-1024 <1007160080@qq.com>
2017-08-24 19:37:54 +0800

    数组和

--- code/FindNumWithSum.h
+++ code/FindNumWithSum.h
@@ -29,6 +29,45 @@ bool FindNumWithSum(int* data, int len, int sum, int& num1, int& num2)
 	return found;
 }
 
+//输入一个正数s，打印出所有和为s的连续正数序列。
+//输入15，  1+2+3+4+5=4+5+6=7+8=15
+
+void PrintFindSeq(int samll, int big)
+{
+	for (int i = samll; i <= big; i++)
+	{
+		cout << i << " ";
+	}
+	cout << endl;
+}
+
+
+void FindSeq(int sum)
+{
+	if (sum < 3)
+		return;
+	int small = 1;
+	int big = 2;
+	int mid = (1 + sum) / 2;
+	int curSum = small + big;
+
+	while (small < mid)
+	{
+		if (curSum == sum)
+			PrintFindSeq(small, big);
+		while (curSum>sum && small < mid)
+		{
+			curSum -= small;
+			small++;
+
+			if (curSum == sum)
+				PrintFindSeq(small, big);
+		}
+		big++;
+		curSum += big;
+	}
+}
+
 void Test()
 {
 	int data[] = { 1, 2, 4, 7, 11, 15 };
@@ -40,4 +79,7 @@ void Test()
 	}
 	else
 		cout << "Not found!" << endl;
+
+	int sum = 15;
+	FindSeq(sum);
 }
